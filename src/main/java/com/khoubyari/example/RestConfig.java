4024607e30b0115ea0763d5412e4cfe5d186b77e
Chae Lee <arfarf89@gmail.com>
2017-03-07 16:43:59 +0900

    Add CORS config

---
+++ src/main/java/com/khoubyari/example/RestConfig.java
@@ -0,0 +1,27 @@
+package com.khoubyari.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by chaelee on 2017. 3. 7..
+ */
+@Configuration
+public class RestConfig {
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
